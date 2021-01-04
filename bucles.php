da4b8b0e4b7cbb21db3e03c343df4a92a5831b31
jenny n <ya.eselin@hotmail.com>
2021-01-03 21:28:04 -0400

    primer commit

---
+++ bucles.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$i = 0; 
+for($i=0;$i<10;$i++)
+{
+echo "i ahora equivale a ".$i."</br> "; 
+}
+echo "Fin del bucle while. El bucle while terminó en ".$i."";
+?>
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+La fecha del día de hoy es: <?= date("d/m/Y"); ?>.
+<br />
+La hora local del servidor es: <?= date("G:i:s"); ?>.
+
+</body>
+</html>
